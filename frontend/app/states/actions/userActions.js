4e7fdd343eac42cfb5b67f144acfd33fe8dff27c
PV <patrickvalera@patMB.lan>
2023-11-12 00:37:24 -0800

    install and initial react redux

---
+++ frontend/app/states/actions/userActions.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import {
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_DETAILS_RESET,
+} from "../constants/userConstants";
+
+const routeLink = "/api/users/";
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST,
+        });
+        const config = {
+            header: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        const { data } = await axios.post(
+            routeLink + "login",
+            { email, password },
+            config
+        );
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        });
+        localStorage.setItem("UserInfo", JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+export const logout = () => async (dispatch) => {
+    try {
+        localStorage.removeItem("UserInfo");
+        dispatch({
+            type: USER_LOGOUT,
+        });
+        dispatch({
+            type: USER_DETAILS_RESET,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
